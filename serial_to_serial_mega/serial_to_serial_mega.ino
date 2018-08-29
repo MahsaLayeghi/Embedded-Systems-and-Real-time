abc6399aef33270949304fb0f0ff2547f5d7d859
MahsaLayeghi <39732491+MahsaLayeghi@users.noreply.github.com>
2018-08-29 21:43:39 +0430

    Add files via upload

---
+++ serial_to_serial_mega/serial_to_serial_mega.ino
@@ -0,0 +1,69 @@
+int ledpin = 13;
+int phy_addr = 10;
+int reg_addr;
+int data;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  pinMode(ledpin,OUTPUT);
+  pinMode(2,INPUT);
+  pinMode(8,OUTPUT);
+  
+  Serial.flush();
+  Serial1.flush();
+}
+
+void loop() {
+    
+    digitalWrite(8,HIGH);
+    Serial.println("Enter phy_addr");
+    while(Serial.available()==0){} 
+    phy_addr = Serial.parseInt();
+  
+    Serial.println("Enter reg_addr");
+    while(Serial.available()==0){} 
+    reg_addr = Serial.parseInt();
+  
+    Serial.println("Enter pattern of led on's");
+    while(Serial.available()==0){} 
+    data = Serial.parseInt();
+  
+    Serial.println();
+
+  switch(phy_addr)
+  {
+    case 10:
+         if(data == 1) digitalWrite(ledpin,HIGH);
+         else digitalWrite(ledpin,LOW);
+         break;
+
+   case 50:
+        Serial1.write(phy_addr/256);
+        Serial1.write(phy_addr%256);
+        Serial1.write(reg_addr/256);
+        Serial1.write(reg_addr%256);
+        Serial1.write(data/256);
+        Serial1.write(data%256);
+        delay(50);
+
+        if(reg_addr>127)//read status of led's
+        {
+          while(Serial1.available()<4){}
+          byte b1 = Serial1.read();
+          byte b2 = Serial1.read();
+          byte b3 = Serial1.read();
+          byte b4 = Serial1.read();
+          Serial.println(b1);
+          Serial.println(b2);
+          Serial.println(b3);
+          Serial.println(b4);
+        }
+        break;
+        
+
+  }
+
+}
---
+++ serial_to_serial_uno/serial_to_serial_uno.ino
@@ -0,0 +1,169 @@
+int phy_addr;
+int reg_addr;
+int data;
+bool swich = false;
+
+void update_leds(){
+    int s4,s5,s6,s7;
+    s4 = digitalRead(4);
+    s5 = digitalRead(5);
+    s6 = digitalRead(6);
+    s7 = digitalRead(7);
+    
+    if(s4 == HIGH)digitalWrite(10,HIGH);
+    else digitalWrite(10,LOW);
+    if(s5 == HIGH)digitalWrite(11,HIGH);
+    else digitalWrite(11,LOW);
+    if(s6 == HIGH)digitalWrite(12,HIGH);
+    else digitalWrite(12,LOW);
+    if(s7 == HIGH)digitalWrite(13,HIGH);
+    else digitalWrite(13,LOW);
+}
+
+void setup() {
+   phy_addr = 50;
+   Serial.begin(9600);
+   pinMode(13,OUTPUT);
+   pinMode(12,OUTPUT);
+   pinMode(11,OUTPUT);
+   pinMode(10,OUTPUT);
+   pinMode(4,INPUT_PULLUP);
+   pinMode(5,INPUT_PULLUP);
+   pinMode(6,INPUT_PULLUP);
+   pinMode(7,INPUT_PULLUP);
+   
+}
+
+void loop() {
+
+    //update_leds();
+    
+    while(Serial.available()<6){}
+  
+    byte b1 = Serial.read();
+    byte b2 = Serial.read(); 
+    byte b3 = Serial.read();
+    byte b4 = Serial.read();
+    byte b5 = Serial.read();
+    byte b6 = Serial.read();
+    
+  
+    phy_addr = (b1*256)+b2;
+    reg_addr = (b3*256)+b4;
+    data = (b5*256)+b6;
+   
+    if(phy_addr == 50)
+    {
+      if(reg_addr<128)
+      {
+        switch(data)
+        {
+          case 0:
+            digitalWrite(13,LOW);
+            digitalWrite(12,LOW);
+            digitalWrite(11,LOW);
+            digitalWrite(10,LOW);
+            break;
+          case 1:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,LOW);
+            digitalWrite(11,LOW);
+            digitalWrite(10,LOW);
+            break;
+          case 2:
+            digitalWrite(13,LOW);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,LOW);
+            digitalWrite(10,LOW);
+            break;
+          case 3:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,LOW);
+            digitalWrite(10,LOW);
+            break;
+          case 4:
+            digitalWrite(13,LOW);
+            digitalWrite(12,LOW);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,LOW);
+            break;
+          case 5:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,LOW);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,LOW);
+            break;
+          case 6:
+            digitalWrite(13,LOW);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,LOW);
+            break;
+          case 7:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,LOW); 
+            break;
+          case 8:
+            digitalWrite(13,LOW);
+            digitalWrite(12,LOW);
+            digitalWrite(11,LOW);
+            digitalWrite(10,HIGH);
+            break;
+          case 9:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,LOW);
+            digitalWrite(11,LOW);
+            digitalWrite(10,HIGH);
+            break;
+          case 10:
+            digitalWrite(13,LOW);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,LOW);
+            digitalWrite(10,HIGH);
+            break;
+          case 11:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,LOW);
+            digitalWrite(10,HIGH);
+            break;
+          case 12:
+            digitalWrite(13,LOW);
+            digitalWrite(12,LOW);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,HIGH);
+            break;
+          case 13:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,LOW);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,HIGH);
+            break;
+          case 14:
+            digitalWrite(13,LOW);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,HIGH);
+            break;
+          case 15:
+            digitalWrite(13,HIGH);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,HIGH);
+            break;
+        }
+      }else{
+        byte b1,b2,b3,b4;
+        b1 = Serial.write(digitalRead(13));
+        b2 = Serial.write(digitalRead(12));
+        b3 = Serial.write(digitalRead(11));
+        b4 = Serial.write(digitalRead(10));
+        delay(50);
+      }
+   }
+}
+    
+
